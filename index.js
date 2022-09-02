e33c7720f53cbb12f1e4d7ba7aba47b0cd4d162d
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-09-02 17:00:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  //loop through string halfway
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    //compare firstletter with last letter
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+
+  console.log("=>", isPalindrome("robot"));
+}
 */
 
 /*
   Add written explanation of your solution here
+
+
 */
 
 // You can run `node index.js` to view these console logs
